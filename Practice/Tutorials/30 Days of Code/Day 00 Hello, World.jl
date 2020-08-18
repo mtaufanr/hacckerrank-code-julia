52fe1ec008ab3373b1265f9df3648f7ed3095da7
Taufan <mtaufan@protonmail.ch>
2020-08-18 15:22:16 +0700

    Create Day 00 Hello, World.jl

---
+++ Practice/Tutorials/30 Days of Code/Day 00 Hello, World.jl
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/30-hello-world/problem
+
+input = readline()
+
+function greet(input_string::String)
+    println("Hello, World.")
+    println(input_string)
+end
+
+greet(input)
