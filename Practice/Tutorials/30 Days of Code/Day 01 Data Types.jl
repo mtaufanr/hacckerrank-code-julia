98dcbd302c2e7c53b80ccb76196079145e8b508b
Taufan <mtaufan@protonmail.ch>
2020-08-18 15:22:19 +0700

    Create Day 01 Data Types.jl

---
+++ Practice/Tutorials/30 Days of Code/Day 01 Data Types.jl
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/30-data-types/problem
+
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+inputs = readlines()
+i = i + parse(Int64, inputs[1])    
+d = d + parse(Float64, inputs[2])
+s = s * " " * inputs[3]
+
+# Print the sum of both integer variables on a new line.
+println(i)
+# Print the sum of the double variables on a new line.
+println(d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+println(s)
