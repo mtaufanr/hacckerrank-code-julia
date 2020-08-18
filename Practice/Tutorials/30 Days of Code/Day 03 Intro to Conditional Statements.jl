6131816d9b3acbaf4600649fd8873bb080b31e2a
Taufan <mtaufan@protonmail.ch>
2020-08-18 16:32:35 +0700

    Create Day 03 Intro to Conditional Statements.jl

---
+++ Practice/Tutorials/30 Days of Code/Day 03 Intro to Conditional Statements.jl
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/30-conditional-statements/problem
+
+n = parse(Int64, readline())
+
+function check_weird(v::Int64)
+    r_weird = "Weird"
+    r_not_weird = "Not Weird"
+    is_even = v % 2 == 0
+    if is_even
+        if 2 ≤ v ≤ 5
+            return r_not_weird
+        elseif 6 ≤ v ≤ 20
+            return r_weird
+        else
+            return r_not_weird
+        end
+    else
+        return r_weird
+    end
+end
+
+println(check_weird(n))
