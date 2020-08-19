1664e4a8b34d947978097fd8e89b7ab577d0faaa
Taufan <mtaufan@protonmail.ch>
2020-08-19 22:24:20 +0700

    Create Day 06 Let's Review.jl

---
+++ Practice/Tutorials/30 Days of Code/Day 06 Let's Review.jl
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/30-review-loop/problem
+
+function get_input(n::Int64)::Array{String}
+    s = String[]
+    for _ in 1:n
+        append!(s, readlines())
+    end
+    return s
+end
+
+function print_sol(sa::Array{String})
+    for i in eachindex(sa)
+        s1 = sa[i][1:2:end]
+        s2 = sa[i][2:2:end]
+        println("$s1 $s2")
+    end
+end
+        
+
+n = parse(Int64, readline())
+s = get_input(n)
+print_sol(s)
